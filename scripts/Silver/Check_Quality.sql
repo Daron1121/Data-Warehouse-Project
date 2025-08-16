f2f533b5ceb7dabd5817af2ad36840dfc23cb21b
Daron1121 <168169947+Daron1121@users.noreply.github.com>
2025-08-16 16:10:44 +0200

    Update and rename Check_Quality to Check_Quality.sql

--- scripts/Silver/Check_Quality.sql
+++ scripts/Silver/Check_Quality.sql
@@ -46,3 +46,36 @@ from Silver.crm_prd_info
 select *
 from Silver.crm_prd_info
 where prd_end_dt < prd_start_dt
+
+select * from Silver.crm_prd_info
+
+-- crm_sales_details
+
+-- Check for Nulls or Duplicates in Primary Key
+-- Expectation: no result
+
+select
+nullif(sls_due_dt, 0)
+from Bronze.crm_sales_details
+where 
+sls_due_dt <= 0
+or len(sls_due_dt) != 8
+or sls_due_dt > 20600101
+or sls_due_dt < 19000101
+
+--Check Data Consistency: Between Sales, Quantity and Price
+-- >> Values must not be Null, zero or negative
+-- >> Sales = Price * Quantity
+
+select distinct
+	sls_sales as old_sls_sales,
+	sls_quantity,
+	sls_price
+from Silver.crm_sales_details
+where sls_price * sls_quantity != sls_sales 
+or sls_price is null 
+or sls_quantity is null
+or sls_sales is null
+or sls_quantity <= 0
+or sls_price <= 0
+or sls_sales <= 0
